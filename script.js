059f28d27d34ac3e4feaa3e559a462f3e4d563ed
jeremyers1 <4466264+jeremyers1@users.noreply.github.com>
2022-06-29 11:32:54 -0700

    Add color picker box

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const container = document.getElementById('container');
+const colorPicker = document.getElementById('color_picker');
+let colors = ["black", "white", "red", "blue", "green", "yellow"];
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 2500; i++) {
     const box = document.createElement('div');
     box.className = 'box';
     container.appendChild(box);
     box.addEventListener('mouseover', draw);
 }
 
+for (let i = 0; i < 6; i++) {
+    const colBox = document.createElement('div');
+    colBox.className = 'color_box';
+    colBox.style = `background-color: ${colors[i]}`;
+    colorPicker.appendChild(colBox);
+    colBox.addEventListener('click', pickColor);
+}
+
 function draw() {
     const color = 'blue';
     this.style.background = color;
+}
+
+function pickColor() {
+
 }
   
 function restart(){
